7f3b2cb720e38cf3a9084c5b4597465e0a9f5876
Philipp Gaßner <philipp.gassner@isaweiden.de>
2023-04-05 15:48:27 +0200

    more stuff but still beta

--- src/components/DatesComponent.vue
+++ src/components/DatesComponent.vue
@@ -1,35 +1,67 @@
 <script>
 export default {
     props: {
-        data: Object
+        data: Object,
+        index: Number
+    },
+    computed: {
+        humandate() {
+            let d = new Date();
+
+            //Converts Seconds to rough Milliseconds
+            d.setTime(this.data.ts * 1000);
+            let str = `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()} - ${d.getHours()}:${d.getMinutes()}`
+            return str;
+        }
     }
 }
 </script>
 
 <template>
-    <div class="row">
+    <div class="row big">
         <!-- Left Comp -->
         <div class="imgContainer">
             <img :src="data.promo">
         </div>
         <!-- Right Comp -->
         <div>
-            {{ data.title }}
+            <h2>
+                {{ data.title }}
+            </h2>
+            <p>
+                {{ humandate }} @ {{ data.where }}
+            </p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
+h2 {
+    margin: 0;
+}
+.big {
+    height: 10em !important;
+    grid-template-columns: 10em 1fr;
+}
+
 .row {
     display: grid;
-    grid-template-columns: 0.25fr 0.75fr;
     grid-template-rows: 1fr;
-    grid-column-gap: 0px;
+    grid-column-gap: 1em;
     grid-row-gap: 0px;
+    height: 2em;
 }
 
 .imgContainer {
-    height: 100%;
+    background-color: green;
+    height: 10em;
+    width: 10em;
+    overflow: hidden;
+}
+
+img {
+    height: 10em;
+    /* height: ; */
     /* background-color: green; */
 }
 </style>
--- src/pages/BookingPage.vue
+++ src/pages/BookingPage.vue
@@ -1,3 +1,77 @@
+<script>
+export default {
+    data() {
+        return {
+            selectedDj: this.$route.query.artist ?? -1
+        }
+    },
+    mounted() {
+        // alert(this.selectedDj);
+    }
+}
+</script>
 <template>
     <h1>Booking</h1>
+
+    <form>
+        <div class="block">
+
+            <label for="artist">DJ/Artist:</label>
+            <select name="artist" v-model="selectedDj" required>
+                <option disabled value="-1">None selected</option>
+                <option v-for="(dj, index) in this.$store.state.artists" :value="index">
+                    {{ dj.name }}
+                </option>
+            </select>
+        </div>
+
+        <!-- <p> -->
+        <div class="block">
+            <label for="name">Name:</label>
+            <input type="text" name="name" required>
+        </div>
+
+        <div class="block">
+            <label for="email">E-Mail Address:</label>
+            <input type="email" name="email" required>
+        </div>
+
+        <div class="block">
+
+            <label for="message">Message:</label>
+            <textarea name="message"></textarea>
+        </div>
+
+        <input type="submit">
+        <!-- </p> -->
+
+        <!-- <p> -->
+
+        <!-- </p> -->
+
+    </form>
 </template>
+
+<style scoped>
+form {
+    /* display: flex; */
+}
+.block {
+    /* width: 49%; */
+    /* display: inline-block; */
+    margin: 1em 0;
+}
+
+/* .right {
+    float: right;
+} */
+
+label,
+input {
+    display: block;
+}
+
+textarea {
+    width: 100%;
+}
+</style>
